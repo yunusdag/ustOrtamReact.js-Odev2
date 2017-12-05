0ddd33542603989cb767f5f694b54f014ec82bef
alisaitbilgi <alisaitbilgi@gmail.com>
2017-12-05 13:14:58 +0300

    Ready for HOMEWORK-1

---
+++ homework-1.md
@@ -0,0 +1,24 @@
+# HOMEWORK 1
+  
+  For your first homework you are responsible for adding "add-task" functionality for our
+  to do list application. When "Add-Task" button is pressed with an input, the written input-text
+  should be added to our list items.
+  
+  ##Requirements: 
+  
+  - Style of this application will "NOT" be considered for homework-1. (you can use default styles)
+  
+  - When add-task button is pressed with an input value, it should be added to the list.
+  
+  - When input field is empty 'add-task' button should 'NOT' add any item to the list.
+  
+  
+  ##Hints: 
+  
+    Just look the comments that i've added to our code-base and when you follow the hints your homework will be done :)
+    You don't need to copy paste any code from anywhere, just follow the hints and remember the flow that we make on 
+    our lesson. 
+    Just be sure any cheating will be detected and will not be tolarated...
+  
+
+# React-ToDo-App
--- src/components/listContainer.js
+++ src/components/listContainer.js
@@ -8,9 +8,9 @@ class ListContainer extends Component {
 
   render() {
     /*
-     After creating same flow that we did our lesson, you will have your todo datas a
-     You will need to loop over this immutable array by using JSX syntax and create dynamic to-do-list here.
-     this is a test immutable array you can look at example and understand how to do the operation.
+      After creating same flow that we did our lesson, you will have your to do datas and then, just loop over
+    this immutable array by using JSX syntax and create dynamic to-do-list here.
+    this is a test immutable array you can look at example and understand how to do the operation.
     */
     const testDatas = I.List([1, 2, 3, 4]);
 
@@ -19,6 +19,7 @@ class ListContainer extends Component {
         {
           /*
             This is test example with test data. You will need to make it dynamic with store's todo data.
+            We are looping our fake immutable array by using 'testdata.map(() => "div creation")' like this.
            */
           testDatas.map(eachTestData => (
             <div key={eachTestData} className="fake-todo-with-test-data">{eachTestData}</div>
